a1e58ec366521a3e2f0e1bece6faedea5fe35459
Muhammad Fahad Hussain Rana <149395895+MuhammadFad@users.noreply.github.com>
2026-04-07 01:46:28 +0500

    ADD Azure CORS Polic

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,9 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5173',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    //Azure Addresses
+    'http://68.210.73.143:3000/'
 ];
 
 app.use(cors({
